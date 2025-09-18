575dd840cdbb1a2325fdc2799623a4af727f16a9
Leon Schäfer <leon.schaefer@zdh.thm.de>
2025-09-18 16:43:19 +0200

    feature: hopefully fix pipeline

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,11 @@
 # needed for vite to have access to the env variables
 cat <<EOF > /usr/share/nginx/html/env.js
 window.ENV = {
-  BASE_URL: "${BASE_URL}",
+    BASE_URL: "${BASE_URL}",
 }
 EOF
+
+# Update Nginx configuration to listen on port 8080
+sed -i 's/listen 80;/listen 8080;/' /etc/nginx/conf.d/default.conf
 
 exec nginx -g "daemon off;"
