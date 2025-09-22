a95746b641a09da02a5c16694cc3f714b106e660
Leon Schäfer <leon.schaefer@zdh.thm.de>
2025-09-22 21:43:20 +0200

    refactor: normalize indent to 4 spaces for all files

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './vitest.setup.mjs',
-  },
-  base: process.env.VITE_BASE_URL || '',
+    plugins: [react(), tsconfigPaths()],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './vitest.setup.mjs',
+    },
+    base: process.env.VITE_BASE_URL || '',
 });
