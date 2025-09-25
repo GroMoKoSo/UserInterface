98df675b442b80cfacae440042fb43ed8dad99fd
Leon Schaefer <leon.schaefer@zdh.thm.de>
2025-09-26 00:40:48 +0200

    feat: add keycloak envs

--- src/types/constants.tsx
+++ src/types/constants.tsx
@@ -5,6 +5,8 @@ declare global {
             VITE_KEYCLOAK_URL?: string
             VITE_USERMANAGEMENT_URL?: string
             VITE_APIMANAGEMENT_URL?: string
+            VITE_KEYCLOAK_REALM?: string
+            VITE_KEYCLOAK_CLIENT_ID?: string
         };
     }
 }
@@ -14,5 +16,5 @@ export const KEYCLOAK_URL = window.ENV?.VITE_KEYCLOAK_URL ?? import.meta.env.VIT
 export const USERMANAGEMENT_URL = window.ENV?.VITE_USERMANAGEMENT_URL ?? import.meta.env.VITE_USERMANAGEMENT_URL
 export const APIMANAGEMENT_URL = window.ENV?.VITE_APIMANAGEMENT_URL ?? import.meta.env.VITE_APIMANAGEMENT_URL
 
-export const KEYCLOAK_REALM = "gromokoso"
-export const KEYCLOAK_CLIENT_ID = "user-interface"
+export const KEYCLOAK_REALM = window.ENV?.VITE_KEYCLOAK_REALM ?? import.meta.env.VITE_KEYCLOAK_REALM
+export const KEYCLOAK_CLIENT_ID = window.ENV?.VITE_KEYCLOAK_CLIENT_ID ?? import.meta.env.VITE_KEYCLOAK_CLIENT_ID
